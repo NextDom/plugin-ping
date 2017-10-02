f4e0374d186bc4cee8faac214afb2570ce19c87d
guenneguezt <thomas@guenneguez.com>
2017-10-02 22:22:48 +0200

    Suppression lien paypal

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -43,15 +43,6 @@ if (!isConnect('admin')) {
 			<a class="btn btn-warning" id="bt_DetectBin" style="color : white;"><i class="fa fa-wrench"></i> {{Detecter les programme ping et arp-scan}}</a>
 		</div>
     </div>
-	<div class="form-group">
-		<label class="col-lg-4 control-label">{{Faire un don au développeur}}</label>
-		<div class="col-lg-5">
-			Ce plugin est gratuit pour que chacun puisse en profiter simplement. Si vous souhaitez tout de même faire un don au développeur du plugin, utilisez le lien suivant.<br>
-			<a class="btn" id="bt_paypal" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=VDCCCHBA3CCSE" target="_new" >
-				<img src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donate_LG.gif" border="0" alt="{{Faire un don via Paypal au développeur}}">
-			</a>
-		</div>
-	</div>
 </form>
 <?php include_file('desktop', 'ping', 'js', 'ping'); ?>
 
