099c14cc579de39c306ffbb3e647d4f83ba4f626
guenneguezt <thomas@guenneguez.com>
2017-11-02 21:22:16 +0100

    Erreur d'icone

--- desktop/php/ping.php
+++ desktop/php/ping.php
@@ -53,7 +53,7 @@ sendVarToJS('eqType', 'ping');
                 foreach ($eqLogics as $eqLogic) {
                     echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
                     echo "<center>";
-                    echo '<img src="plugins/ping/doc/images/ping_icon.png" height="105" width="95" />';
+                    echo '<img src="plugins/ping/plugin_info/ping_icon.png" height="105" width="95" />';
                     echo "</center>";
                     echo ' <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"> <center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
                     echo '</div>';
